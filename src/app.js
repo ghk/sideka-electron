8d426e28a5b78977ce6ad4ad98e4449b8cc52531
Gozali Kumara <ghk@microvac.co.id>
2016-10-09 21:12:36 +0700

    basic spreadsheet

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@
 import os from 'os'; // native node.js module
 import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
+import Handsontable from 'handsontable';
 import { greet } from './hello_world/hello_world'; // code authored by you in this project
 import env from './env';
+import penduduk from './penduduk-fulur';
 
 console.log('Loaded environment variables:', env);
 
@@ -18,7 +20,48 @@ var appDir = jetpack.cwd(app.getAppPath());
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = env.name;
+    var data = [
+        ["", "Ford", "Volvo", "Toyota", "Honda"],
+        ["2016", 10, 11, 12, 13],
+        ["2017", 20, 11, 14, 13],
+        ["2018", 30, 15, 12, 13]
+    ];
+    var container = document.getElementById('sheet');
+    var hot = new Handsontable(container, {
+        data: penduduk,
+        rowHeaders: true,
+        colHeaders: [
+              'NIK',
+              'Nama Penduduk',
+              'Tempat Lahir',
+              'Tanggal Lahir',
+              'Jenis Kelamin',
+              'No Telp',
+              'Email',
+              'No Kitas',
+              'No Paspor',
+              'Golongan Darah',
+              'RT',
+              'RW',
+              'Nama Dusun',
+              'Pendidikan',
+              'Pendidikan Terakhir',
+              'Agama',
+              'Status Kawin',
+              'Pekerjaan',
+              'Pekerjaan PED',
+              'Kewarganegaraan',
+              'Kompetensi',
+              'Status Penduduk',
+              'Status Tinggal',
+              'Difabilitas',
+              'Kontrasepsi',
+              'No KK',
+              'Alamat Jalan',
+              'Nama Ayah',
+              'Nama Ibu',
+              'Status Keluarga',
+          ],
+          fixedColumnsLeft: 2,
+    });
 });
