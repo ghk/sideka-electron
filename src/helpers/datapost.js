c67d160fbd1d17bb80555064ff726e11aac278e6
Gozali Kumara <ghk@microvac.co.id>
2016-10-15 20:52:55 +0700

    image can't offline

--- src/helpers/datapost.js
+++ src/helpers/datapost.js
@@ -2,13 +2,11 @@ import request from 'request';
 import path from 'path';
 import { remote } from 'electron'; 
 import jetpack from 'fs-jetpack'; // module loaded from npm
-import crypto from 'crypto';
 import $ from 'jquery';
 
 var app = remote.app;
 var DATA_DIR = app.getPath("userData");
 var CONTENT_DIR = path.join(DATA_DIR, "contents");
-var IMAGE_DIR = path.join(DATA_DIR, "feed_images");
 jetpack.dir(CONTENT_DIR);
 
 var dataapi = {
@@ -18,20 +16,13 @@ var dataapi = {
     },
     
     getImage(div, url, callback){
-        var md5 = crypto.createHash('md5');
-        var fileName = md5.update(url).digest("hex")+".jpg";
-        var fullFileName = path.join(IMAGE_DIR, fileName);
-        if (jetpack.exists(fullFileName)){
-            callback(fullFileName);
-        } else {
-            $.get(url, function(html){
-                $(div).html("").append($(html));
-                var ogImage = $("meta[property='og:image']", div).attr("content");
-                if(ogImage){
-                    callback(ogImage);
-                }
-            });
-        }
+        $.get(url, function(html){
+            $(div).html("").append($(html));
+            var ogImage = $("meta[property='og:image']", div).attr("content");
+            if(ogImage){
+                callback(ogImage);
+            }
+        });
     },
 
     getContent: function(type){
