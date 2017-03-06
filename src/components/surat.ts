ce86b31f9921b1be4a46ca22a176af537b9b7975
wm-mrx <wm.mrx2015@gmail.com>
2017-03-06 18:45:40 +0700

    add domisili form print

--- src/components/surat.ts
+++ src/components/surat.ts
@@ -5,6 +5,7 @@ import { remote, shell } from 'electron'; // native electron module
 import * as fs from 'fs';
 import createPrintVars from '../helpers/printvars';
 
+var $ = require('jquery');
 var jetpack = require('fs-jetpack'); 
 var Docxtemplater = require('docxtemplater');
 var path = require('path');
@@ -24,43 +25,45 @@ export default class SuratComponent{
     result: any[];
     keyword: string;
     hot: any;
+    selectedLetter: any;
 
     constructor(){   
         this.loadLetters();  
+        this.selectedLetter = {"formData": {}};
     }
     
     loadLetters(): void {
         this.letters = [];
 
         this.letters.push({"name": 'kartu keluarga', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/kk.docx', "form": null });
+        "path": 'surat_templates/kk.docx', "form": null, "formData": {} });
 
-        this.letters.push({"name": 'keterangan domisili', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/keterangan-domisili.docx', "form": null  });
+        this.letters.push({"name": 'keterangan domisili', "thumbnail": '../surat_templates/ket_domisili.png', 
+        "path": 'surat_templates/ket_domisili.docx', "form": 'domisili-form', "formData": {}});
 
-        this.letters.push({"name": 'keterangan kelahiran', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/keterangan-kelahiran.docx', "form": null  });
+        this.letters.push({"name": 'keterangan kelahiran', "thumbnail": '../surat_templates/ket_kelahiran.png', 
+        "path": 'surat_templates/ket_kelahiran.docx', "form": null, "formData": {}  });
 
-        this.letters.push({"name": 'keterangan umum', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/ket_umum.docx', "form": null  });
+        this.letters.push({"name": 'keterangan umum', "thumbnail": '../surat_templates/ket_umum.png', 
+        "path": 'surat_templates/ket_umum.docx', "form": null, "formData": {}  });
 
-        this.letters.push({"name": 'pindah datang wni', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/pindah-datang-wni.docx', "form": null  });
+        this.letters.push({"name": 'pindah datang wni', "thumbnail": '../surat_templates/pindah_datang_wni.png', 
+        "path": 'surat_templates/pindah_datang_wni.docx', "form": null, "formData": {}  });
 
         this.letters.push({"name": 'pelaporan kematian', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/pelaporan-kematian.docx', "form": null  });
+        "path": 'surat_templates/pelaporan-kematian.docx', "form": null, "formData": {}  });
 
         this.letters.push({"name": 'pindah kelamin', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/pelaporan-kematian.docx', "form": null  });
+        "path": 'surat_templates/pelaporan-kematian.docx', "form": null, "formData": {}  });
 
         this.letters.push({"name": 'pindah agama', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/pelaporan-kematian.docx', "form": null });
+        "path": 'surat_templates/pelaporan-kematian.docx', "form": null, "formData": {} });
 
         this.letters.push({"name": 'pindah keluarga', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/pelaporan-kematian.docx', "form": null  });
+        "path": 'surat_templates/pelaporan-kematian.docx', "form": null, "formData": {}  });
 
         this.letters.push({"name": 'pindah pasangan', "thumbnail": '../surat_thumbnails/kk.png', 
-        "path": 'surat_templates/pelaporan-kematian.docx', "form": null  });
+        "path": 'surat_templates/pelaporan-kematian.docx', "form": null, "formData": {}  });
 
         this.result = this.letters;
     }
@@ -86,6 +89,32 @@ export default class SuratComponent{
     }
 
     print(letter: any): boolean {
+        this.selectedLetter = letter;
+
+        if(letter.form)
+            $('#' + letter.form).modal('show');
+        else
+            this.doPrint();
+
+        return false;
+    }
+
+    doPrint(): void{
+        switch(this.selectedLetter.name){
+            case 'keterangan umum':
+              this.printKeteranganUmum(this.selectedLetter);
+            break;
+            case 'kartu keluarga':
+              this.printKK(this.selectedLetter);
+            break;
+            case 'keterangan domisili':
+              this.printKeteranganDomisili(this.selectedLetter);
+            break;
+        }
+    }
+
+    printKeteranganUmum(data: any): void {
+        var letter = data;
         var selected = this.hot.getSelected();
 
         if(!selected)
@@ -149,7 +178,78 @@ export default class SuratComponent{
                 app.relaunch();
             });
         }
+    }
+
+    printKK(data: any): void {
         
-        return false;
+    }
+
+    printKeteranganDomisili(data: any): void {
+        var letter = data;
+        var selected = this.hot.getSelected();
+
+        $('#' + data.form).modal('hide');
+
+         if(!selected)
+            return;
+
+        var fileName = remote.dialog.showSaveDialog({
+            filters: [
+                {name: 'Word document', extensions: ['docx']},
+            ]
+        });
+
+        if(fileName){
+            if(!fileName.endsWith(".docx"))
+                fileName = fileName+".docx";
+
+            var angularParser= function(tag){
+                var expr=expressions.compile(tag);
+                return {get:expr};
+            }
+
+            var nullGetter = function(tag, props) {
+                return "";
+            };
+            var penduduk = schemas.arrayToObj(this.hot.getDataAtRow(selected[0]), schemas.penduduk);
+            var content = fs.readFileSync(letter.path, "binary");
+
+            let dataFile = path.join(app.getPath("userData"), "setting.json");
+
+            if(!jetpack.exists(dataFile))
+                return null;
+
+            let data = JSON.parse(jetpack.read(dataFile));
+            let that = this;
+           
+            dataapi.getDesa(function(desas){
+                var auth = dataapi.getActiveAuth();
+                var desa = desas.filter(d => d.blog_id == auth['desa_id'])[0];
+                var printvars = createPrintVars(desa);
+
+                let opts = { 
+                    "centered": false, 
+                    "getImage": (tagValue) => {
+                        return tagValue;
+                    }, 
+                    "getSize": (image, tagValue, tagName) => {
+                        return [100, 100];
+                    } 
+                };
+
+                letter.formData.judul = 'DOMISILI';
+                var imageModule = new ImageModule(opts);   
+                var doc = new Docxtemplater(content);
+
+                doc.setOptions({parser:angularParser, nullGetter: nullGetter});
+                doc.setData({penduduk: penduduk, vars: printvars, form: letter.formData, image: that.convertDataURIToBinary(data.logo)});
+                doc.render();
+
+                var buf = doc.getZip().generate({type:"nodebuffer"});
+                fs.writeFileSync(fileName, buf);
+                shell.openItem(fileName);
+                app.relaunch();
+            });
+        }
     }
 }
