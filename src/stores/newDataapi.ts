91b0846666639546d1f1ff31aeca29ac536e77c6
wm-mrx <wm.mrx2015@gmail.com>
2017-03-15 11:07:29 +0700

    modified newDataapi.ts

--- src/stores/newDataapi.ts
+++ src/stores/newDataapi.ts
@@ -12,17 +12,7 @@ var app = remote.app;
 
 const SERVER = 'https://api.sideka.id';
 const DATA_DIR = app.getPath("userData");
-const PENDUDUK_DIR = path.join(DATA_DIR, "penduduk");
-const DESA_DIR = path.join(DATA_DIR, "desa");
-const APBDES_DIR = path.join(DATA_DIR, "apbdes");
 const CONTENT_DIR = path.join(DATA_DIR, "contents");
-
-const PENDUDUK_DIR_FILE = {
-    SERVER_DATA: path.join(PENDUDUK_DIR, 'server_data.json'),
-    LOCAL_DATA: path.join(PENDUDUK_DIR, 'local_data.json'),
-    CHANGE_IDS: path.join(PENDUDUK_DIR, 'change_ids.json'),
-    DIFFS: path.join(PENDUDUK_DIR, 'diffs.json')
-}
 
 jetpack.dir(CONTENT_DIR);
 
@@ -41,160 +31,153 @@ let getHttpHeaders = () => {
     return { "X-Auth-Token": token, "X-Sideka-Version": pjson.version }; 
 }
 
+interface Diff {
+    updated: any[];
+    deleted: any[];
+    added: any[];
+    total: number;
+}
+
+interface Content{
+    data: any;
+    columns: any;
+    changeId: number;
+    diffs: Diff[]
+}
+
 class NewDataApi{
     constructor(){}
 
-    getPenduduks(callback): void{
+    getContent(type, subType, data, schema, dir, callback): void {
+        const CHANGE_IDS_PATH = path.join(dir, 'changes_ids.json');
+        const LOCAL_DATA_PATH = path.join(dir, 'local_data.json');
+        const DIFF_PATH = path.join(dir, 'diffs.json');
+
         let auth = getActiveAuth();
         let headers = getHttpHeaders();
-        let changeId: number = 0; //full content
-       
-        if(PendudukLocalHandler.isChangeExists())
-            changeId = PendudukLocalHandler.loadLatestChangeId(); //get full content + diffs
+        let changeId = 0;
         
-        let URL =  SERVER + "/content/" + auth['desa_id'] + "/penduduk?changeId=" + changeId;
-        let content = {
+        if(LocalHandler.isChangeExists(CHANGE_IDS_PATH))
+           changeId = LocalHandler.loadChangeId(CHANGE_IDS_PATH);
+
+        let content: Content = {
             data: null,
-            columns: schemas.penduduk.map(e => e.field),
-            changeId: null,
+            columns: schema.map(s => s.field),
+            changeId: changeId,
             diffs: []
         }
 
-        request({ method: 'GET', url: URL, headers: getHttpHeaders() }, (err, resp, body) => {
-            //Oops, there is something wrong.. Let's get data from local file
+        let url = SERVER + "/content/" + auth['desa_id'] + "/" + type + "?changeId=" + changeId;
+        
+        if(subType)
+            url = SERVER + "/content/" + auth['desa_id'] + "/" + type + "/" + subType + "?changeId=" + changeId;
+
+        request({ method: 'GET', url: url, headers: headers }, (err, resp, body) => {
             if(err || resp.statusCode !== 200){
-                content = PendudukLocalHandler.loadLocalFileData();
+                content = LocalHandler.loadLocalData(LOCAL_DATA_PATH);
                 callback(content);
                 return;
             }
 
-            //Yeeeiy! latest content is successfully loaded!
             content = JSON.parse(body);
 
-            //Now let's check some diffs from content
-            //Wow, there are some diffs inside here.. let's merge them!
             if(content.diffs.length > 0)
-              content = PendudukLocalHandler.mergeDiff(content.diffs);
-            
-            //Consider current data as local data
-            PendudukLocalHandler.storeLocalData(content);
-            PendudukLocalHandler.storeChangeId(content.changeId);
+               content = this.mergeDiff(DIFF_PATH, content);
 
-            //Final content is ready sir!
+            LocalHandler.storeLocalData(LOCAL_DATA_PATH, content);
+            LocalHandler.storeChangeId(CHANGE_IDS_PATH, content.changeId);
+
             callback(content);
         });
     }
 
-    savePenduduk(data, callback): void {
+    saveContent(type, subType, data, schema, dir, callback): void {
+        const CHANGE_IDS_PATH = path.join(dir, 'changes_ids.json');
+        const LOCAL_DATA_PATH = path.join(dir, 'local_data.json');
+        const DIFF_PATH = path.join(dir, 'diffs.json');
+
         let auth = getActiveAuth();
         let headers = getHttpHeaders();
-        let currentDiffs = PendudukLocalHandler.findDiff(data);
-
+        let currentDiff = this.mergeDiff(dir, data);
+     
         let content: any = {
             data: data,
             columns: schemas.penduduk.map(e => e.field),
-            diffs: currentDiffs,
-            changeId: PendudukLocalHandler.loadLatestChangeId()
+            diffs: [currentDiff],
+            changeId: LocalHandler.loadChangeId(CHANGE_IDS_PATH)
         }
 
-        const URL = SERVER + "/content/" + auth['desa_id'] + "/penduduk";
+        let url = SERVER + "/content/" + auth['desa_id'] + "/" + type;
 
-        request({ method: 'POST', url: URL, headers: headers, json: content}, (err, resp, body) => {
-            //Oops there is something wrong, let's consider this as a diff
+        if(subType)
+            url= SERVER + "/content/" + auth['desa_id'] + "/" + type + "/" + subType;
+
+        request({ method: 'POST', url: url, headers: headers, json: content}, (err, resp, body) => {
             if(err || resp.statusCode !== 200){
-                PendudukLocalHandler.storeDiff(data);
-                return;
+                LocalHandler.storeDiff(DIFF_PATH, currentDiff);
             }
-            
-            //Yeiy content has been saved! let's change local file stuffs
+
             else{
-                PendudukLocalHandler.storeChangeId(resp['id']);
-            
-                //and store current data as server data and local data as well
-                //wait, let's add latest change id to current content
-                content.changeId = resp['id'];
-                PendudukLocalHandler.storeServerData(content);
-                PendudukLocalHandler.storeLocalData(content);
+                currentDiff.changeId = resp['id'];
+                LocalHandler.storeChangeId(CHANGE_IDS_PATH, currentDiff.changeId);
+                LocalHandler.storeLocalData(LOCAL_DATA_PATH, currentDiff);
             }
-           
+        
             if(callback)
-                callback(err, resp, body);
+                callback(currentDiff);
         });
+    }
+    
+    mergeDiff(dir, currentData): any {
+        const LOCAL_DATA_PATH = path.join(dir, 'local_data.json');
+        let localData = LocalHandler.loadLocalData(LOCAL_DATA_PATH);
+        return {};
     }
 }
 
-class PendudukLocalHandler{
-    static storeDiff(diff): void {
+class LocalHandler{
+    static storeDiff(filePath, data): void{
         let diffs: any[] = [];
 
-        if(jetpack.exists(PENDUDUK_DIR_FILE.DIFFS))
-          diffs = JSON.parse(jetpack.read(PENDUDUK_DIR_FILE.DIFFS));
+        if(jetpack.exists(filePath))
+          diffs = JSON.parse(jetpack.read(filePath));
         
-        diffs.push(diff);
+        diffs.push(data);
 
-        jetpack.write(PENDUDUK_DIR_FILE, JSON.stringify(diffs));
+        jetpack.write(filePath, JSON.stringify(diffs));
     }
 
-    static loadDiffs(): any {
-        if(jetpack.exists(PENDUDUK_DIR_FILE.DIFFS))
-            return JSON.parse(jetpack.read(PENDUDUK_DIR_FILE.DIFFS));
-
-        return [];
+    static storeLocalData(filePath, data): void{
+        jetpack.write(filePath, JSON.stringify(data));
     }
 
-    static findDiff(data): any{
-        let localData = PendudukLocalHandler.loadLocalFileData();
-        
-        return {};
-    }
-
-    static mergeDiff(diffs): any {
-        return {};
-    }
-
-    static storeChangeId(id): void{
+    static storeChangeId(filePath, id): void {
         let changeIds: any[] = [];
 
-        if(jetpack.exists(PENDUDUK_DIR_FILE.CHANGE_IDS))
-          changeIds = JSON.parse(jetpack.read(PENDUDUK_DIR_FILE.CHANGE_IDS));
+        if(jetpack.exists(filePath))
+          changeIds = JSON.parse(jetpack.read(filePath));
         
         changeIds.push(id);
 
-        jetpack.write(PENDUDUK_DIR_FILE, JSON.stringify(changeIds));
+        jetpack.write(filePath, JSON.stringify(changeIds));
     }
 
-    static loadLocalFileData(): any{
-        if(jetpack.exists(PENDUDUK_DIR_FILE.LOCAL_DATA))
-            return JSON.parse(jetpack.read(PENDUDUK_DIR_FILE.LOCAL_DATA));
-
-        return {};
-    }
-
-    static loadLatestChangeId(): any {
-        let changes: any[] = JSON.parse(jetpack.read(PENDUDUK_DIR_FILE.CHANGE_IDS));
-        return changes[changes.length - 1];
-    }
-
-    static isChangeExists(): boolean{
-        if(!jetpack.read(PENDUDUK_DIR_FILE.CHANGE_IDS))
+    static isChangeExists(filePath): boolean{
+        if(!jetpack.read(filePath))
             return false;
 
         return true;
+    } 
+
+    static loadChangeId(filePath): any{
+        let changes: any[] = JSON.parse(jetpack.read(filePath));
+        return changes[changes.length - 1];
     }
 
-    static storeServerData(data): void{
-        jetpack.write(PENDUDUK_DIR_FILE.SERVER_DATA, JSON.stringify(data));
+    static loadLocalData(filePath): any{
+        if(jetpack.exists(filePath))
+            return JSON.parse(jetpack.read(filePath));
+
+        return {};
     }
-
-    static storeLocalData(data): void{
-        jetpack.write(PENDUDUK_DIR_FILE.LOCAL_DATA, JSON.stringify(data));
-    }
-}
-
-class DesaLocalHandler{
-
-}
-
-class ApbdesLocalHandler{
-
 }
