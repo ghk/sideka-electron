3d3e92f496d60a9713ffa1895d0ffecc4ccad70d
wm-mrx <wm.mrx2015@gmail.com>
2017-05-08 18:26:55 +0700

    revert statistic

--- src/pages/penduduk.ts
+++ src/pages/penduduk.ts
@@ -147,6 +147,43 @@ export default class PendudukComponent {
         return false;
     }
 
+    setStatistics(): boolean {
+        this.activeSheet = Sheet.statistik;
+
+        let chart = new PendudukChart();
+        let sourceData = this.sheetComponent.getHotSheet(Sheet.penduduk).getSourceData();
+
+        let pekerjaanRaw = chart.transformRaw(sourceData, 'pekerjaan', 9);
+        let pekerjaanData = chart.transformDataStacked(pekerjaanRaw, 'pekerjaan');
+        let pekerjaanChart = chart.renderMultiBarHorizontalChart('pekerjaan', pekerjaanData);
+        
+        let pendidikanRaw = chart.transformRaw(sourceData, 'pendidikan', 6);
+        let pendidikanData = chart.transformDataStacked(pendidikanRaw, 'pendidikan');
+        let pendidikanChart = chart.renderMultiBarHorizontalChart('pendidikan', pendidikanData);
+
+        let ageGroupRaw = chart.transformAgeGroup(sourceData);
+        let ageGroupData = chart.transformDataPyramid(ageGroupRaw);
+        let ageGroupChart = chart.renderMultiBarHorizontalChart('ageGroup', ageGroupData);
+
+        let agamaRaw = chart.transformRaw(sourceData, 'agama', 7);
+        let agamaData = chart.transformData(agamaRaw, 'agama');
+        let agamaChart = chart.renderPieChart('agama', agamaData);
+
+        let statusKawinRaw = chart.transformRaw(sourceData, 'statusKawin', 8);
+        let statusKawinData = chart.transformData(statusKawinRaw, 'statusKawin');
+        let statusKawinChart = chart.renderPieChart('statusKawin', statusKawinData);
+
+        setTimeout(() => {
+            pekerjaanChart.update();
+            pendidikanChart.update();
+            agamaChart.update();
+            statusKawinChart.update();
+            ageGroupChart.update();
+        }, 3000);
+
+        return false;
+    }
+
     openSaveDialog(): void{
         let type = this.sheetComponent.getType(this.activeSheet);
         let data = this.sheetComponent.data[type];
