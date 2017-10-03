5cee0a4e1809f69916d19dd4d1e98374f6bdc49d
Gozali Kumara <ghk@microvac.co.id>
2017-10-03 09:40:46 +0700

    update readme

--- src/components/pageInfo.ts
+++
@@ -1,8 +0,0 @@
-import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core'
-
-@Component({
-    selector: 'page-info',
-    templateUrl: 'templates/pageInfo.html'
-})
-export default class PageInfoComponent {
-}
