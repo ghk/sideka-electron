c96c95f6cb8d7c956b44efe121be481e8cb6a563
tesla-1st-code <firstcode2017@gmail.com>
2017-11-15 02:51:52 +0700

    lower case sidekaProdeskelMapper

--- src/app/helpers/sidekaProdeskelMapper.ts
+++ src/app/helpers/sidekaProdeskelMapper.ts
@@ -1,4 +1,4 @@
-const GENDER = {'Laki - laki': 'Laki-Laki', 'Perempuan': 'Perempuan'};
+const GENDER = {'Laki - laki': 'Laki-Laki', 'Perempuan': 'Perempuan', 'L': 'Laki-Laki', 'P': 'Perempuan', '1': 'Laki-Laki', '2': 'Perempuan'};
 const NATIONALITY = {'WNI': 'Warga Negara Indonesia', 'WNA': 'Warga Negara Asing', 'DWIKEWARGANEGARAAN': 'Dwi Kewarganegaraan'};
 const RELIGION = {'Aliran Kepercayaan Lainnya': 'Kepercayaan Kepada Tuhan YME'};
 const MARITAL_STATUS = {'Cerai Hidup': 'Janda/Duda', 'Cerai Mati': 'Janda/Duda'};
@@ -23,7 +23,14 @@ const EDUCATION =  {'Tidak Pernah Sekolah': 'Tidak pernah sekolah',
                     'Tamat D-3/Sederajat': 'Tamat D-3/sedarajat',
                     'Tamat S-1/Sederajat': 'Tamat S-1/sederajat',
                     'Tamat S-2/Sederajat': 'Tamat S-2/sederajat',
-                    'Tamat S-3/Sederajat': 'Tamat S-3/sederajat'
+                    'Tamat S-3/Sederajat': 'Tamat S-3/sederajat',
+                    'Tidak/Belum Sekolah': 'Belum masuk TK/Kelompok Bermain',
+                    'SD/Sederajat': 'Sedang SD/sederajat',
+                    'SLTP/Sederajat': 'Sedang SLTP/sederajat',
+                    'SLTA/Sederajat': 'Sedang SLTA/sederajat',
+                    'BELUM TAMAT SD/SEDERAJAT': 'Sedang SD/sederajat',
+                    'Diploma I/II': 'Sedang D-3/sederajat',
+                    'Diploma I/III': 'Sedang D-3/sederajat'
                   };
 
 const JOB = {'BELUM/TIDAK BEKERJA': 'Belum Bekerja',
@@ -117,58 +124,134 @@ const JOB = {'BELUM/TIDAK BEKERJA': 'Belum Bekerja',
 
 export default class SidekaProdeskelMapper {
     static mapGender(data): any {
-        if(GENDER[data])
-            return GENDER[data];
+        let lowerType = SidekaProdeskelMapper.getLowerValues('GENDER');
+
+        if(lowerType[data.toLowerCase()])
+            return lowerType[data.toLowerCase()];
 
         return data;
     }
 
     static mapEducation(data): any {
-        if(EDUCATION[data])
-            return EDUCATION[data];
+        let lowerType = SidekaProdeskelMapper.getLowerValues('EDUCATION');
 
+        if(lowerType[data.toLowerCase()])
+            return lowerType[data.toLowerCase()];
+       
         return data;
     }
 
     static mapJob(data): any {
-        if(JOB[data])
-            return JOB[data];
+        let lowerType = SidekaProdeskelMapper.getLowerValues('JOB');
 
+        if(lowerType[data.toLowerCase()])
+            return lowerType[data.toLowerCase()];
+       
         return data;
     }
 
     static mapFamilyRelation(data): any {
-        if(FAMILY_REL[data])
-            return FAMILY_REL[data];
+         let lowerType = SidekaProdeskelMapper.getLowerValues('FAMILY_REL');
 
+        if(lowerType[data.toLowerCase()])
+            return lowerType[data.toLowerCase()];
+       
         return data;
     }
 
     static mapBloodType(data): any {
-        if(BLOOD_TYPE[data])
-            return BLOOD_TYPE[data];
+         let lowerType = SidekaProdeskelMapper.getLowerValues('BLOOD_TYPE');
 
+        if(lowerType[data.toLowerCase()])
+            return lowerType[data.toLowerCase()];
+       
         return data;
     }
 
     static mapReligion(data): any {
-        if(RELIGION[data])
-            return RELIGION[data];
+         let lowerType = SidekaProdeskelMapper.getLowerValues('RELIGION');
 
+        if(lowerType[data.toLowerCase()])
+            return lowerType[data.toLowerCase()];
+       
         return data;
     }
 
     static mapNationality(data): any {
-        if(NATIONALITY[data])
-            return NATIONALITY[data];
+       let lowerType = SidekaProdeskelMapper.getLowerValues('NATIONALITY');
 
+        if(lowerType[data.toLowerCase()])
+            return lowerType[data.toLowerCase()];
+       
         return data;
     }
 
      static mapMaritalStatus(data): any {
-        if(MARITAL_STATUS[data])
-            return MARITAL_STATUS[data];
+       let lowerType = SidekaProdeskelMapper.getLowerValues('MARITAL_STATUS');
 
+        if(lowerType[data.toLowerCase()])
+            return lowerType[data.toLowerCase()];
+       
         return data;
+    }
+
+    static getLowerValues(type): any {
+        let typeKeys = [];
+        let lowerKeys = {};
+
+        switch(type) {
+            case 'GENDER':
+              typeKeys = Object.keys(GENDER);
+              
+              typeKeys.forEach(typeKey => {
+                 lowerKeys[typeKey.toLowerCase()] = GENDER[typeKey];
+              });
+              break;
+            case 'EDUCATION':
+              typeKeys = Object.keys(EDUCATION);
+              typeKeys.forEach(typeKey => {
+                 lowerKeys[typeKey.toLowerCase()] = EDUCATION[typeKey];
+              });
+              break;
+            case 'JOB':
+              typeKeys = Object.keys(JOB);
+               typeKeys.forEach(typeKey => {
+                 lowerKeys[typeKey.toLowerCase()] = JOB[typeKey];
+              });
+              break;
+            case 'FAMILY_REL':
+              typeKeys = Object.keys(FAMILY_REL);
+               typeKeys.forEach(typeKey => {
+                 lowerKeys[typeKey.toLowerCase()] = FAMILY_REL[typeKey];
+              });
+              break;
+            case 'BLOOD_TYPE':
+              typeKeys = Object.keys(BLOOD_TYPE);
+              typeKeys.forEach(typeKey => {
+                 lowerKeys[typeKey.toLowerCase()] = BLOOD_TYPE[typeKey];
+              });
+              break;
+            case 'RELIGION':
+              typeKeys = Object.keys(RELIGION);
+              typeKeys.forEach(typeKey => {
+                 lowerKeys[typeKey.toLowerCase()] = RELIGION[typeKey];
+              });
+              break;
+            case 'MARITAL_STATUS':
+              typeKeys = Object.keys(MARITAL_STATUS);
+              typeKeys.forEach(typeKey => {
+                 lowerKeys[typeKey.toLowerCase()] = MARITAL_STATUS[typeKey];
+              });
+              break;
+            case 'NATIONALITY':
+              typeKeys = Object.keys(NATIONALITY);
+            typeKeys.forEach(typeKey => {
+                 lowerKeys[typeKey.toLowerCase()] = NATIONALITY[typeKey];
+              });
+              break;
+        }
+    
+
+        return lowerKeys;
     }
 }
